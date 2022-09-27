787aec71337289c52d52ae12b705dba48e805ed6
Hung Hoang <Hunghoang159bmt@gmail.com>
2022-09-27 17:38:53 +0700

    117. Xây dựng UI phần sidebar #3

---
+++ src/component/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,40 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './AccountPreview.module.scss';
+import Button from '~/component/Button';
+
+const cx = classNames.bind(styles);
+
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header')}>
+                <img
+                    className={cx('avata')}
+                    src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-aiso/65d3c6b1d1e205c75536ccf1f26d552d~c5_100x100.jpeg?x-expires=1664438400&x-signature=ZyeNTbwbA6cCPpvlUnrgYb9%2BAO8%3D"
+                    alt=""
+                />
+                <Button className={cx('follow-btn')} primary>
+                    Follow
+                </Button>
+            </div>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>Nguyenvana</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Nguyen Van A</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Follower</span>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
--- src/layouts/components/Sidebar/Menu/MenuItems.js
+++ src/layouts/components/Sidebar/Menu/MenuItems.js
@@ -1,7 +1,6 @@
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
